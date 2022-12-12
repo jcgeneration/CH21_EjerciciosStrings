89a4e3a37565ff6576834aa4097a2d7b635dc36f
jc <juan.valencia@idr.edu.mx>
2022-12-12 16:08:02 -0600

    se agregan las funciones: reves y esPalindromo

--- js/main.js
+++ js/main.js
@@ -18,3 +18,49 @@ for (let index = 0; index < nombre.length; index++) {
 console.log(letra, contFinal);
 
 
+
+//Entrada
+//"Laura Aline"
+//Salida
+//"enilA aruaL"
+
+function reves(str){
+    let tmpStr="";
+    for (let indice = str.length-1; indice >=0; indice--) {
+        tmpStr += str.charAt(indice);
+    }//for  indice
+    return tmpStr;
+}//reves
+
+
+/*
+Palíndromo: Palabra o expresión que es igual si se lee de izquierda a derecha que de derecha a izquierda.
+Amma
+Otto
+Anina
+Natan
+Ana
+Ada
+Siris
+Aziza
+Ramar
+Nayan
+
+Radar
+Reconocer
+anita lava la tina
+
+*/
+
+
+function esPalindromo(str){
+    str =str.toLowerCase();
+    str = str.replaceAll(" ", "");
+    if (reves(str)==str) 
+        return true;
+        
+    return false;
+}//esPalindromo
+
+
+
