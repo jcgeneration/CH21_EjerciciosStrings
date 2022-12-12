d70edb8e780dd1c0adc154b4255ded0cfdda3c1f
jc <juan.valencia@idr.edu.mx>
2022-12-12 13:47:10 -0600

    Se soluciona el primer ejercicio con Strings

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
 //Aquí se escribiran los ejercicios con Strings
+let nombre ="Valeria Duque" ; //"Abraham";
+nombre  = nombre.toUpperCase();
+let letra="";
+let contFinal=0;
+for (let index = 0; index < nombre.length; index++) {
+    console.log(nombre.charAt(index));
+    let contTemp=1;
+    for (let cont = index+1; cont < nombre.length; cont++) {
+        if(nombre.charAt(index) == nombre.charAt(cont))
+            contTemp++;
+    } // for cont
+    if (contTemp>contFinal) {
+        contFinal=contTemp;
+        letra = nombre.charAt(index);
+    }//contTemp>contFinal
+}// for index
+console.log(letra, contFinal);
+
+
