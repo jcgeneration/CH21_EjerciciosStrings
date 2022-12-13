ca3e5fd87ed1e8ddb33e62ef1aef3321dcd6ed04
jc <juan.valencia@idr.edu.mx>
2022-12-13 10:54:36 -0600

    Se agegran validaciones a campo de correo y texto

--- js/main.js
+++ js/main.js
@@ -1,1 +1,35 @@
 //Aquí se escribiran los ejercicios con Strings
+let btnEnviar = document.getElementById("btnEnviar");
+btnEnviar.addEventListener("click", function(event){
+    event.preventDefault();
+    //https://emailregex.com/
+    let emailRegex =/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/g;
+
+    let exampleFormControlInput1 = document.getElementById("exampleFormControlInput1"); //email
+    let exampleFormControlInput2 = document.getElementById("exampleFormControlInput2");  //RFC
+    let exampleFormControlTextarea1 = document.getElementById("exampleFormControlTextarea1");
+    let alertError = document.getElementById("alertError");
+    exampleFormControlTextarea1.value = exampleFormControlTextarea1.value.trim();
+    alertError.style.display="none";
+    alertError.innerHTML="";
+    if (exampleFormControlTextarea1.value.trim().replaceAll("  " , "").length < 20){
+        alertError.innerHTML = "El mensaje debe contener 20 caracteres o más.";
+        alertError.style.display="block";
+        exampleFormControlTextarea1.focus();
+        exampleFormControlTextarea1.select();
+    }//if
+
+    if (exampleFormControlInput1.value.match(emailRegex) == null) {
+        alertError.style.display="block";
+        alertError.innerHTML += "<br/>El correo electrónico no es válido.";
+    }//
+    let RFCRegex = / /;
+    if (exampleFormControlInput2.value.match(RFCRegex) == null) {
+        alertError.style.display="block";
+        alertError.innerHTML += "<br/>El RFC no es válido.";
+    }//
+
+
+
+
+});
