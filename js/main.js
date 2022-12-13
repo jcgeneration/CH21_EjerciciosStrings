d518f2811e49481aa6e02e2a906f7e4a0f324456
jc <juan.valencia@idr.edu.mx>
2022-12-13 12:42:57 -0600

    Se agrega un setTimeout para quitar bordes verdes y un clearTimeout para evitar errores

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 //Aquí se escribiran los ejercicios con Strings
 let btnEnviar = document.getElementById("btnEnviar");
+let idTimeout;
+
 btnEnviar.addEventListener("click", function(event){
     event.preventDefault();
+    let validos=0;
+
     //https://emailregex.com/
     let emailRegex =/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/g;
 
@@ -17,21 +21,42 @@ btnEnviar.addEventListener("click", function(event){
         alertError.style.display="block";
         exampleFormControlTextarea1.focus();
         exampleFormControlTextarea1.select();
+        exampleFormControlTextarea1.style.border = "solid red 1px";
+    }else {
+        exampleFormControlTextarea1.style.border = "solid green 1px";
+        validos++;
     }//if
 
     if (exampleFormControlInput1.value.match(emailRegex) == null) {
         alertError.style.display="block";
         alertError.innerHTML += "<br/>El correo electrónico no es válido.";
-    }//
+        exampleFormControlInput1.style.border = "solid red 1px";
+    }else{
+        exampleFormControlInput1.style.border = "solid green 1px";
+        validos++;
+    }
     let RFCRegex = /^[A-Z]{4}[0-9]{6}[A-Z0-9]{3}$/;
     //  /^[A-Z]{4}\d{6}\w{3}$/;
      exampleFormControlInput2.value = exampleFormControlInput2.value.toUpperCase();
     if (exampleFormControlInput2.value.match(RFCRegex) == null) {
         alertError.style.display="block";
         alertError.innerHTML += "<br/>El RFC no es válido.";
-    }//
+        exampleFormControlInput2.style.border = "solid red 1px";
+    }else {
+        exampleFormControlInput2.style.border = "solid green 1px";
+        validos++;
+        //exampleFormControlInput2.style.border = "";
+    } //else
+    if ((idTimeout!=undefined) && (idTimeout!=null)){
+        clearTimeout(idTimeout);
+    }// idTimeout
 
-
-
-
+    if (validos == 3 ){
+        idTimeout = setTimeout (function(){
+            exampleFormControlTextarea1.style.border ="";
+            exampleFormControlInput1.style.border ="";
+            exampleFormControlInput2.style.border =""; 
+        }, 3000);
+        console.log(idTimeout);
+    }//==3
 });
